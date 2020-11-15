ee066e7ef3faf8ebaba8239ce75d26d86f64c91b
Imsamx <imsamx@gmail.com>
2020-11-16 00:22:27 +0300

    Pr3

---
+++ MCS_JS_Basics/Pr3/js/script.js
@@ -0,0 +1,9 @@
+document.body.style.backgroundColor = "#696969";
+const Colored = document.querySelector('.page');
+Colored.style['background-color'] = 'ghostwhite';
+const Span = document.querySelector('.name');
+Span.innerHTML = "DJ Rashad";
+const Img = document.querySelector('img');
+Img.src = "https://thumbnailer.mixcloud.com/unsafe/900x900/extaudio/9/8/b/c/21e8-83e3-4f35-9a95-e6c53ea57bf8.png";
+const ShortB = document.querySelector('.short-bio');
+ShortB.className += ' animated';
